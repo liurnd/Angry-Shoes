2d2c988775cb6dc26fb177d42091a71f10103a5c
yangmeng <yangmeng@yangmeng-laptop.(none)>
2011-05-30 15:28:46 +0800

    Upload obj and env

--- env.h
+++ env.h
@@ -1,19 +1,38 @@
-#ifndef ENV_H
-#define ENV_H
-#include<vector>
-#include"mathUtil.h"
 
-class obj;
-class env
+#ifndef WORLD_H
+#define WORLD_H
+
+#include <vector>
+#include <map>
+#include "MathUtils.h"
+#include "Arbiter.h"
+
+struct Body;
+struct Joint;
+
+struct World
 {
-	private:
-		int fvckyou;
-	public:
-		vec g;
-		std::vector<obj*> list;
+	
+	std::vector<Body*> list;
+	std::vector<Joint*> joints;
+	std::map<ArbiterKey, Arbiter> arbiters;
+	Vec2 g;
+	int iterations;
+	static bool accumulateImpulses;
+	static bool warmStarting;
+	static bool positionCorrection;
+	
+	
+	World(Vec2 gravity, int iterations) :g(gravity), iterations(iterations) {}
 
-		void destory(obj* o);
-		void update(float dt);
-		void add(obj* o);
+	void add(Body* body);
+	void add(Joint* joint);
+	void destroy();
+
+	bool update(float dt);
+
+	void BroadPhase();
+
+	
 };
 #endif
--- obj.h
+++ obj.h
@@ -1,21 +1,36 @@
+
 #ifndef OBJ_H
 #define OBJ_H
-#include"mathUtil.h"
 
-class obj
+#include "MathUtils.h"
+#define obj Body
+struct Body
 {
-	//What the hell ever you like
-	public:
-		
-		virtual void destory(obj& byWho);
-		virtual void hit(obj& byWho);
+	Vec2 position;
+	float rotation;
 
-		vec pos;
-		int typeID;
-		vec shape;
-		float theta;
-		float HP;
-		float mass;
+	Vec2 velocity;
+	float angularVelocity;
+
+	Vec2 force;
+	float torque;
+
+	Vec2 width;
+
+	float friction;
+	float mass, invMass;
+	float I, invI;
+	
+	Body();
+	void Set(const Vec2& w, float m,const Vec2& pos,const float ro);
+
+	void AddForce(const Vec2& f)
+	{
+		force += f;
+	}
+
+	virtual void destroy(obj& byWho);
+	virtual void hit(obj& byWho);	
 };
 
 #endif
